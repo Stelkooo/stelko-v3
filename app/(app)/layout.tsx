b454c2b6e3420c0bf65e0048406c4a7668eda620
stelko <stilyanmtoskov@gmail.com>
2024-01-08 22:35:58 +0000

    fetching header data

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,18 @@
 import Header from '@/components/global/header/header.global';
+import { sanityFetch } from '@/sanity/lib/fetch';
+import { siteQuery } from '@/sanity/lib/queries';
+import { TSite } from '@/types';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const site = await sanityFetch<TSite>({ query: siteQuery, tags: ['site'] });
+
   return (
     <div className="flex min-h-[100dvh] flex-col">
-      <Header />
+      <Header header={site?.header} />
       <main className="flex-1">{children}</main>
     </div>
   );
---
+++ sanity/lib/queries.ts
@@ -0,0 +1,221 @@
+import { groq } from 'next-sanity';
+
+const linkQuery = groq`
+  _key,
+  linkType,
+  openNewWindow,
+  linkParameters,
+  linkType == "internal" => {
+    internal-> {
+      slug,
+      _type,
+    },
+  },
+  linkType == "external" => {
+    external,
+  }
+`;
+
+const navLinkQuery = groq`
+  _key,
+  title,
+  link {
+    ${linkQuery},
+  } 
+`;
+
+const headerQuery = groq`
+  "header": *[_type == "header"][0] {
+    cta {
+      ${navLinkQuery},
+    },
+    navLinks[] {
+      ${navLinkQuery},
+    },
+  }
+`;
+
+const footerQuery = groq`
+  "footer": *[_type == "footer"][0] {
+    navLinks[] {
+      ${navLinkQuery},
+    },
+    policies[] {
+      ${navLinkQuery},
+    },
+    "socials": *[_type == "general"][0].socials,
+  }
+`;
+
+export const siteQuery = groq`
+  {
+    ${headerQuery},
+    ${footerQuery},
+  }
+`;
+
+const imageQuery = groq`
+  _key,
+  "alt": asset->altText,
+  asset-> {
+    ...,
+    metadata,
+  },
+  sizes,
+  "caption": asset->description
+`;
+
+const moduleBaseQuery = groq`
+  _key,
+  _type
+`;
+
+const testimonialQuery = groq`
+  _id,
+  name,
+  testimonial
+`;
+
+const modulesQuery = groq`
+  _type == "heroModule" => {
+    ${moduleBaseQuery},
+    heading,
+    subheading,
+    heroType,
+    heroType == 'textAboveImage' => {
+      image {
+        ${imageQuery},
+      },
+    },
+  },
+  _type == "layoutModule" => {
+    ${moduleBaseQuery},
+    layoutType,
+    heading,
+    subheading,
+    layoutType == "twoColumnCards" => {
+      twoColumnCards[] {
+        _key,
+        heading,
+        text,
+      },
+    },
+    layoutType == "threeColumnCards" => {
+      threeColumnCards[] {
+        _key,
+        heading,
+        text,
+      },
+    },
+    layoutType == "imageTextCard" => {
+      imageTextCard {
+        heading,
+        text,
+        image {
+          ${imageQuery},
+        },
+        imageOnLeft,
+      },
+    },
+    layoutType == "oneImageTwoTextCards" => {
+      oneImageTwoTextCards {
+        imageCard {
+          image {
+            ${imageQuery},
+          },
+          heading,
+          text,
+          cta {
+            ${navLinkQuery},
+          },
+        },
+        textCards[] {
+          _key,
+          heading,
+          text,
+          cta {
+            ${navLinkQuery},
+          },
+        },
+      },
+    },
+  },
+  _type == 'faqModule' => {
+    ${moduleBaseQuery},
+    heading,
+    text,
+    cta {
+      ${navLinkQuery},
+    },
+    faqs[] {
+      question,
+      answer,
+      _key,
+    },
+  },
+  _type == "testimonialModule" => {
+    ${moduleBaseQuery},
+    testimonialType,
+    testimonialType == 'single' => {
+      testimonial-> {
+        ${testimonialQuery},
+      },
+    },
+    testimonialType == 'multiple' => {
+      heading,
+      text,
+      testimonials[]-> {
+        ${testimonialQuery},
+      },
+    },
+  },
+  _type == "ctaModule" => {
+    ${moduleBaseQuery},
+    heading,
+    text,
+    ctas {
+      primaryCta {
+        ${navLinkQuery},
+      },
+      secondaryCta {
+        ${navLinkQuery},
+      },
+    },
+  },
+  _type == "imageModule" => {
+    ${moduleBaseQuery},
+    imageType,
+    imageType == "single" => {
+      image {
+        ${imageQuery},
+      },
+    },
+    imageType == "double" => {
+      images[] {
+        ${imageQuery},
+      },
+    },
+  }
+`;
+
+export const homeQuery = groq`
+  *[_type == "home" && _id == "home"][0] {
+    modules[] {
+      ${modulesQuery},
+    },
+  }
+`;
+
+export const homeSeoQuery = groq`
+  *[_type == "home" && _id == "home"][0] {
+    seoAndSocial,
+    publishStatus,
+  }
+`;
+
+export const homeSitemapQuery = groq`
+  *[_type == "home" && _id == "home"][0] {
+    _type,
+    _updatedAt,
+  }
+`;
