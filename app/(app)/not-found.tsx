ed98f23bc77e8899c5a20ee4e0f81fb879e5af54
stelko <stilyanmtoskov@gmail.com>
2024-01-18 11:35:33 +0000

    404 page

---
+++ app/(app)/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <section>
+      <div className="container my-20 flex flex-col items-center gap-8">
+        <h1>404</h1>
+        <p>How you managed to get here then?</p>
+      </div>
+    </section>
+  );
+}
